f0c6e2859855d7d735a5dad0d455f16692a7203a
Luke Simmons <luke.simmons@gmail.com>
2017-10-24 22:11:23 +0200

    update intermediates so that columns are now correct

--- CertificateAuthorities.py
+++ CertificateAuthorities.py
@@ -8,28 +8,92 @@ import re
 
 class CertificateAuthorities():
 
-  def __init__(self, url, ca_type, relative_path ):
-    self.url = url
-    # whether this a root or intermediate
+  def __init__(self, ca_type, relative_path ):
+    self.roots_url = 'https://ccadb-public.secure.force.com/mozilla/IncludedCACertificateReportPEMCSV'
+    self.intermediates_url = 'https://ccadb-public.secure.force.com/mozilla/PublicAllIntermediateCertsWithPEMCSV'
+
+    self.url = None
+
     self.ca_type = ca_type
     self.relative_path = relative_path
-    self.root_columns = ["owner","certificate_issuer_organization","certificate_issuer_organizational_unit","common_name_or_certificate_name", "certificate_serial_number", "sha_256_fingerprint", "certificate_id", "valid_from_gmt", "valid_to_gmt", "public_key_algorithm", "signature_hash_algorithm", "trust_bits", "ev_policy_oid", "approval_bug", "nss_release_when_first_included", "firefox_release_when_first_included", "test_website_valid", "mozilla_applied_constraints", "company_website", "geographic_focus", "certificate_policy", "certification_practice_statement", "standard_audit", "br_audit", "ev_audit", "auditor", "standard_audit_type", "standard_audit_statement_dt", "pem_info"] 
-    self.intermediate_columns = ["ca_owner","parent_name","certificate_name","certificate_issuer_common_name","certificate_issuer_organization","certificate_serial_number","sha_256_fingerprint","certificate_id","valid_from_gmt","valid_to_gmt","public_key_algorithm","signature_hash_algorithm","extended_key_usage","cp_cps_same_as_parent","certificate_policy","certification_practice_statement","audits_same_as_parent","standard_audit","br_audit","auditor","standard_audit_statement_dt","management_assertions_by","comments","pem_info"]
+
+    self.root_columns = [
+      "owner",
+      "certificate_issuer_organization",
+      "certificate_issuer_organizational_unit",
+      "common_name_or_certificate_name",
+      "certificate_serial_number", 
+      "sha_256_fingerprint", 
+      "certificate_id", "valid_from_gmt", 
+      "valid_to_gmt", 
+      "public_key_algorithm", 
+      "signature_hash_algorithm", 
+      "trust_bits", 
+      "ev_policy_oid", 
+      "approval_bug", 
+      "nss_release_when_first_included", 
+      "firefox_release_when_first_included", 
+      "test_website_valid", 
+      "mozilla_applied_constraints", 
+      "company_website", 
+      "geographic_focus", 
+      "certificate_policy", 
+      "certification_practice_statement", 
+      "standard_audit", 
+      "br_audit", 
+      "ev_audit", 
+      "auditor", 
+      "standard_audit_type", 
+      "standard_audit_statement_dt", 
+      "pem_info"
+    ] 
+
+    self.intermediate_columns = [
+      "ca_owner", #CA Owner
+      "parent_name", #Parent Name
+      "certificate_name", # Certificate Name
+      "certificate_issuer_common_name", # Certificate Issuer Common Name
+      "certificate_issuer_organization", # Certificate Issuer Organization
+      "certificate_subject_common_name", # Certificate Subject Common Name
+      "certificate_subject_organization", # Certificate Subject Organization
+      "certificate_serial_number", # Certificate Serial Number
+      "sha_256_fingerprint", # SHA-256 Fingerprint
+      "certificate_id", # Certificate ID
+      "valid_from_gmt", # Valid From [GMT]
+      "valid_to_gmt", # Valid To [GMT]
+      "public_key_algorithm", # Public Key Algorithm
+      "signature_hash_algorithm", # Signature Hash Algorithm
+      "extended_key_usage", # Extended Key Usage
+      "cp_cps_same_as_parent", # CP/CPS Same As Parent
+      "certificate_policy", # Certificate Policy (CP)
+      "certification_practice_statement", # Certification Practice Statement (CPS)
+      "audits_same_as_parent", # Audits Same As Parent
+      "standard_audit", # Standard Audit
+      "br_audit", # BR Audit
+      "auditor", # Auditor
+      "standard_audit_statement_dt", # Standard Audit Statement Dt
+      "management_assertions_by", # Management Assertions By
+      "comments", # Comments
+      "pem_info" # PEM Info
+   ]
 
   def __get_result_dictionary(self):
     result = defaultdict(list)
-    csv_file = urllib.request.urlopen(self.url)
-    datareader = csv.reader(io.TextIOWrapper(csv_file))
-    i=0
 
     if self.ca_type == "root":
+      self.url = self.roots_url
       self.columns = self.root_columns
       self.cn_field = "common_name_or_certificate_name"
     elif self.ca_type == "intermediate":
+      self.url = self.intermediates_url
       self.columns = self.intermediate_columns
       self.cn_field = "certificate_name"
     else:
       sys.exit('ca_type must be "root" or "intermediate". Your value is: "' + ca_type + '"')
+
+    csv_file = urllib.request.urlopen(self.url)
+    datareader = csv.reader(io.TextIOWrapper(csv_file))
+    i=0
 
     for row in datareader:
       # skip the first row, as these are simply the headers
